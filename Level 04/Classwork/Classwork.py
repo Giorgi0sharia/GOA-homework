6c632048698f87aa9954b35d99180a602b71a473
Giorgi0sharia <gio.shariaman@gmail.com>
2024-10-18 19:39:23 +0400

    GOA

---
+++ Level 04/Classwork/Classwork.py
@@ -0,0 +1,60 @@
+Var1 = "GOA"
+Var2 = "GOA"
+
+num1 = 3
+num2 = 4
+
+anum1 = 3.14
+anum2 = 4.67
+
+print(Var1)
+print(Var2)
+
+print(num1)
+print(num2)
+
+print(anum1)
+print(anum2)
+
+
+#meore davaleba
+
+print(4 + 4)
+print(10+ 4)
+print(4 + 5)
+print(56 + 7)
+print(99 + 100)
+
+print(4 - 4)
+print(10 - 4)
+print(4 - 5)
+print(56 - 7)
+print(99 - 100)
+
+print(4 * 4)
+print(10 * 4)
+print(4 * 5)
+print(56 * 7)
+print(99 * 100)
+
+print(4 / 4)
+print(10 / 4)
+print(4 / 5)
+print(56 / 7)
+print(99 / 100)
+
+print(4 % 4)
+print(10 % 4)
+print(4 % 5)
+print(56 % 7)
+print(99 % 100)
+
+print(4 ** 4)
+print(10 ** 4)
+print(4 ** 5)
+print(56 ** 7)
+print(99 ** 100)
+
+#mesame davaleba
+
+print((10 % 4) * (100 / 98) + 9 - (10**2))
---
+++ Level 04/Homework/Homework.py
@@ -0,0 +1,47 @@
+#Pirveli davaleba
+
+print(4 + 9)
+print(10+ 4)
+print(100 + 5)
+
+print(4 - 11)
+print(10 - 80)
+print(420- 4)
+
+print(4 * 69)
+print(10 * 420)
+print(4 * 59)
+
+print(100 / 4)
+print(10 / 434)
+print(41 / 512)
+
+print(19 % 461)
+print(10 % 13985)
+print(4 % 1823)
+
+print(5 ** 4)
+print(8 ** 4)
+print(100 ** 6)
+
+#meore davaleba
+
+celcius = 37
+
+fahrenheit = (celcius*9 /5) + 32
+
+print(fahrenheit)
+
+#mesame davaleba
+
+me = 15
+da = 14
+mama = 51
+deda = 52
+
+#oci wlis shemdeg
+
+print(me + 20)
+print(da + 20)
+print(mama + 20)
+print(deda + 20)
