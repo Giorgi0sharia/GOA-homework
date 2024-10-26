2cb08217a225218686e11698686e0f9f60fd995b
Giorgi0sharia <gio.shariaman@gmail.com>
2024-10-26 11:21:11 +0400

    GOA

---
+++ Level 06/Classwork/Classwork.py
@@ -0,0 +1,92 @@
+num1 = 10
+num1 += 20
+print(num1)
+num12 = 14
+num12 += 21
+print(num12)
+num13 = 4
+num13 += 3
+print(num1)
+num14 = 13
+num14 += 201
+print(num1)
+num15 = 12
+num15 += 20384
+print(num1)
+
+num2 = 30
+num2 /= 4
+print(num2)
+num22 = 305
+num22 /= 42
+print(num22)
+num23 = 377
+num23 /= 45
+print(num23)
+num4 = 302
+num4 /= 412
+print(num4)
+num25 = 30
+num25 /= 4
+print(num25)
+
+
+
+
+
+
+num3 = 420
+num3 -= 20
+print(num3)
+num32 = 42032
+num32 = 2032
+print(num32)
+num34 = 42023
+num34 -= 2012
+print(num34)
+num35 = 4223
+num35 -= 2023
+print(num35)
+
+
+num4 = 96
+num4 *= 4
+print(num4)
+num42 = 9623
+num42 *= 421
+print(num42)
+num43 = 9623
+num43 *= 4232
+print(num43)
+num44 = 9623
+num44 *= 4232
+print(num44)
+num45 = 96232
+num45 *= 4212
+print(num45)
+
+print = "Hello World"
+print("variable")
+print("Hello World")
+print("This is an indented line")
+undeclared_variable = 3
+print(undeclared_variable)
+print("Hello World")
+x = 10
+y = 5
+print(x + y)
+def my_function():
+ 
+print("Unclosed string")
+print("Escaped quote" )
+True = 5
+print(True)
+# Missing comment symbol
+#This is a comment
+print("No colon in if statement")
+def add(a, b):
+ a + b
+print("Empty statement after variable declaration")
+
+
+
---
+++ Level 06/Homework/Homework.py
@@ -0,0 +1,218 @@
+#10-10 magaliti operatorebit.
+
+num1 = 10
+num1 += 20
+print(num1)
+num12 = 14
+num12 += 21
+print(num12)
+num13 = 4
+num13 += 3341
+print(num13)
+num14 = 13
+num14 += 201
+print(num14)
+num15 = 12
+num15 += 20384
+print(num15)
+num16 = 120
+num16 += 203
+print(num16)
+num17 = 1452
+num17 += 21
+print(num17)
+num18 = 4123
+num18 += 3364
+print(num18)
+num19 = 13123
+num19 += 201132
+print(num19)
+num20 = 12123
+num20 += 20384
+print(num20)
+
+num2 = 30
+num2 /= 4
+print(num2)
+num22 = 305
+num22 /= 42
+print(num22)
+num23 = 377
+num23 /= 45
+print(num23)
+num24 = 302
+num24 /= 412
+print(num24)
+num25 = 30
+num25 /= 4
+print(num25)
+num26 = 120123
+num26 /= 203235
+print(num26)
+num27 = 1452
+num27 /= 2132
+print(num27)
+num28 = 412323
+num28 /= 33643223
+print(num28)
+num29 = 1312332
+num29 /= 201132
+print(num29)
+num30 = 1212323
+num30 /= 20384544
+print(num30)
+
+
+num3 = 420
+num3 -= 20
+print(num3)
+num32 = 42032
+num32 -= 2032
+print(num32)
+num34 = 42023
+num34 -= 2012
+print(num34)
+num35 = 4223
+num35 -= 2023
+print(num35)
+num36 = 120
+num36 -= 2032
+print(num16)
+num37 = 145223
+num37 -= 2145
+print(num17)
+num38 = 4123
+num38 -= 3364
+print(num38)
+num39 = 13123
+num39 -= 20113232
+print(num39)
+num40 = 1212323
+num40 -= 2038412
+print(num40)
+
+
+
+num4 = 96
+num4 *= 4
+print(num4)
+num42 = 9623
+num42 *= 421
+print(num42)
+num43 = 9623
+num43 *= 4232
+print(num43)
+num44 = 9623
+num44 *= 4232
+print(num44)
+num45 = 96232
+num45 *= 4212
+print(num45)
+num46 = 120
+num46 *= 203
+print(num46)
+num47 = 1452
+num47 *= 2123
+print(num47)
+num48 = 4123
+num48 *= 3364
+print(num18)
+num49 = 13123
+num49 *= 201132
+print(num49)
+num50 = 12123
+num50 *= 20384
+print(num50)
+
+num5 = 96
+num5 /= 4
+print(num5)
+num52 = 9623
+num52 /= 421
+print(num52)
+num53 = 9623
+num53 /= 4232
+print(num53)
+num54 = 9623
+num54 /= 4232
+print(num54)
+num55 = 96232
+num55 /= 4212
+print(num55)
+num56 = 120
+num56 /= 203
+print(num56)
+num57 = 1452
+num57 /= 21
+print(num57)
+num58 = 4123
+num58 /= 3364
+print(num58)
+num59 = 13123
+num59 /= 201132
+print(num59)
+num60 = 12123
+num60 /= 20384
+print(num60)
+
+#meore davaleba
+
+variable = 10
+variable = "potato"
+variable = "GOA"
+variable = 100
+variable = "house"
+print(variable)
+
+#mesame davaleba
+
+case_insensitive = "Ada, Fortran, SQL, Pascal"
+case_sensitive = "C#, Java, Ruby, XML."
+print(case_insensitive , case_sensitive)
+
+#meotxe davaleba
+
+from turtle import *
+
+color("brown")
+begin_fill()
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(400)
+left(90)
+forward(200)
+left(90)
+forward(200)
+end_fill()
+
+penup() 
+goto(50, 0)
+pendown()
+
+color("purple")
+begin_fill()
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+end_fill()
+
+penup() 
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+left(180)
+left(45)
+forward(285)
+left(90)
+forward(285)
+end_fill()
+
+
+exitonclick()
