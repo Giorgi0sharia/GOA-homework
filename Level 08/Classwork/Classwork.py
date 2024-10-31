67687baebca2110fea2690cf24b19ebdd77a57af
Giorgi0sharia <gio.shariaman@gmail.com>
2024-10-31 20:35:14 +0400

    GOA

---
+++ Level 08/Classwork/Classwork.py
@@ -0,0 +1,32 @@
+name = input("Enter your name:")
+surname = input("Enter your surname:")
+age = input("Enter your age:")
+number = input("Enter a number:")
+number2 = input("Enter another number")
+
+print(name)
+print(surname)
+print(age)
+print(number)
+print(number2)
+
+num1 = input("Enter a number:")
+num2 = input("Enter another number:")
+
+print(num1 + num2)
+
+name_data_type = (type(name))
+print(name_data_type)
+surname_data_type = (type(surname))
+print(surname_data_type)
+age_data_type = (type(age))
+print(age_data_type)
+number_data_type = (type(number))
+print(number_data_type)
+number2_data_type = (type(number2))
+print(number2_data_type)
+num1_data_type = (type(num1))
+print(num1_data_type)
+num2_data_type = (type(num2))
+print(num2_data_type)
+
---
+++ Level 08/Homework/Homework.py
@@ -0,0 +1,30 @@
+#პირველი დავალება
+
+#ცვლადების შექმნა
+name = input("Enter your name please: ")
+surname = input("Enter your surname please: ")
+
+#ეკრანზე პრინტით გამოტანა
+print("hello " + name + " " + surname)
+
+#მეორე დავალება
+
+#ვქმნით ცვლადებს
+num1 = 10
+num2 = 4.5
+num3 = 420
+num4 = 25
+num5 = 17
+
+#ვწერთ მაგალითებს
+print(num1 * num2 - num3 / num4 + num5 % (num1 ** num4))
+print(num3*num4 + num1 ** num5 % num2 - num2)
+print(num1 ** num2 / num3 % num4 **num5 -num1)
+print(num1 - num2 +num3 %num4 **num5 *num3)
+print((num1+num2)*(num2-num3)/(num3 ** num4)% num5 * num2 )
+print(num2 - num1 ** num4 % num5 *num1 + num4)
+print(num5 * num2 + num1 %num4 -num5 *num1)
+print(num1 % num3 *num4 /num3 -num2 +num1)
+print(num2 - num1 %num3 *num4 **num5 +num1)
+print(num3 + num4 - num5 % num2 **num1 *num4 / num2)
+print(num1+num2-num3/num4%num5**num1*num2)
