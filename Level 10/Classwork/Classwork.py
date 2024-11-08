5e242aed3309fd7a6a2bed1d2d8d5bdd1f9dd736
Giorgi0sharia <gio.shariaman@gmail.com>
2024-11-08 20:57:34 +0400

    GOA

---
+++ Level 10/Classwork/Classwork.py
@@ -0,0 +1,71 @@
+#pirveli davaleba, cvladebis gamoyenebit
+
+
+#vqmnit cvladebs
+num1 = 100
+num2 = 89
+num3 = 4130
+num4 = 184
+num5 = 293
+
+#magalitebi
+print(num1 > num2)
+print(num2 > num3)
+print(num3 > num4)
+print(num4 > num5)
+print(num5 > num1)
+
+print(num1 < num2)
+print(num2 < num3)
+print(num3 < num4)
+print(num4 < num5)
+print(num5 < num1)
+
+print(num1 <= num2)
+print(num2 <= num3)
+print(num3 <= num4)
+print(num4 <= num5)
+print(num5 <= num1)
+
+
+print(num1 >= num2)
+print(num2 >= num3)
+print(num3 >= num4)
+print(num4 >= num5)
+print(num5 >= num1)
+
+print(num1 == num2)
+print(num2 == num3)
+print(num3 == num4)
+print(num4 == num5)
+print(num5 == num1)
+
+print(num1 != num2)
+print(num2 != num3)
+print(num3 != num4)
+print(num4 != num5)
+print(num5 != num1)
+
+#meore davaleba
+age = int(input("Enter your current age: "))
+age_in_10yrs = age + 10
+print(age_in_10yrs)
+
+#mesame davaleba
+person1 = int(input("Enter Person1's age: "))
+person2 = int(input("Enter Person2's age: "))
+person3 = int(input("Enter Person3's age: "))
+average = (person1 + person2 + person3)/3
+print(average)
+
+#meotxe davaleba
+number1 = int(input("Please input a number: "))
+number2 = int(input("Please input a second number: "))
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 ** number2)
+print(number2 ** number1)
+print(number1 / number2)
+print(number1 % number2)
---
+++ Level 10/Homework/Homework.py
@@ -0,0 +1,61 @@
+input1 = input("input anything: ")
+input2 = input("input anything: ")
+input3 = input("input anything: ")
+
+print(int(input1))
+print(int(input2))
+print(int(input3))
+
+Weight = float(input("Input your weight: "))
+Height = float(input("Input your Height: "))
+
+BMI = (Weight / Height**2)
+print(BMI)
+
+num1 = 10
+num2 = 30
+num3 = 102149
+num4 = 847
+num5 = 2938
+
+comparison = num1 > num2
+print(comparison)
+comparison2 = num2 > num3
+print(comparison2)
+comparison3 = num3 > num4
+print(comparison3)
+comparison4 = num4 > num5
+print(comparison4)
+comparison5 = num5 > num1
+print(comparison5)
+
+print(num1 < num2)
+print(num2 < num3)
+print(num3 < num4)
+print(num4 < num5)
+print(num5 < num1)
+
+print(num1 <= num2)
+print(num2 <= num3)
+print(num3 <= num4)
+print(num4 <= num5)
+print(num5 <= num1)
+
+
+print(num1 >= num2)
+print(num2 >= num3)
+print(num3 >= num4)
+print(num4 >= num5)
+print(num5 >= num1)
+
+print(num1 == num2)
+print(num2 == num3)
+print(num3 == num4)
+print(num4 == num5)
+print(num5 == num1)
+
+print(num1 != num2)
+print(num2 != num3)
+print(num3 != num4)
+print(num4 != num5)
+print(num5 != num1)
