6f7a62e43295e330993502972b7f2d304df7a8ca
Giorgi0sharia <gio.shariaman@gmail.com>
2024-11-15 22:29:28 +0400

    GOA

---
+++ Level 12/Classwork/Classwork.py
@@ -0,0 +1,25 @@
+print(True or True)
+#true
+print(False or True)
+#true
+print(True or False)
+#true
+print(False or False)
+#false
+
+print(True and True)
+#true
+print(False and True)
+#false
+print(True and False)
+#false
+print(False and False)
+#false
+
+
+num1 = int(input("input a number:"))
+num2 = int(input("input a number:"))
+
+print(num1 > 30)
+print(num2 > 40)
+print(num1 > num2)
---
+++ Level 12/Homework/Homework.py
@@ -0,0 +1,33 @@
+#pirveli davaleba
+
+print(True and True and False or True)
+print(False or True and False or True)
+print(True or False and False and True)
+print(False and True and True or False or True)
+print(False or True and False or True or True and False)
+print(True and True and True or True)
+print(False or False or False or False or False or False or True)
+print(True and True and False and True and True)
+print(False  or True and True and False or True)
+print(True and True and False or True)
+print(False and False and False or True)
+
+#meore davaleba
+
+print(True or True)
+#true
+print(False or True)
+#true
+print(True or False)
+#true
+print(False or False)
+#false
+
+print(True and True)
+#true
+print(False and True)
+#false
+print(True and False)
+#false
+print(False and False)
+#false
