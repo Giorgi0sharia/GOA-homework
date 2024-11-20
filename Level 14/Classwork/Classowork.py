6536b09c26007abb66df88293a99262e09cfa059
Giorgi0sharia <gio.shariaman@gmail.com>
2024-11-20 15:39:45 +0400

    GOA

---
+++ Level 14/Classwork/Classowork.py
@@ -0,0 +1,19 @@
+for i in range(20):
+    print("giorgi sharia")
+
+num1 = int(input("Please type a number: "))
+num2 = int(input("Please type another number: "))
+
+for i in range(num1 , num2):
+    print(i*i)
+    print(i**i)
+    print(i+i)
+    print(i-i)
+    print(i/i)
+
+num3 , num4 = 20 , 70
+
+n  = int(input("enter a number:"))
+
+for i in range(n):
+    print(i**2)
---
+++ Level 14/Homework/Homework.py
@@ -0,0 +1,49 @@
+#pirveli davaleba
+var1 = range(20 , 50)
+#meore davaleba
+var2 = range(100 , 150)
+#mesame davaleba
+var3 = range(20 , 50 , 2)
+#meotxe davaleba
+var4 = range(11 , 31 , 3)
+#mexute davaleba
+for i in range(10):
+    print("Giorgi Sharia")
+#meeqvse davaleba
+for i in range(10 , 30):
+    print(i*2)
+#meshvid davaleba
+for i in range(40 , 60):
+    print(i**3)
+#merve davaleba
+num1 = int(input("Input the first number: "))
+num2 = int(input("Input the second number: "))
+num3 = int(input("Input the third number: "))
+num4 = int(input("Input the fourth number: "))
+num5 = int(input("Input the fifth number: "))
+
+for i in range(5):
+    print(num1)
+    print(num2)
+    print(num3)
+    print(num4)
+    print(num5)
+#mecxre davaleba
+n = input("Enter your name: ")
+for i in n:
+    print(i)
+#meate davaleba
+num6 = int(input("Input the sixth number: "))
+num7 = int(input("Input the seventh number: "))
+num8 = int(input("Input the eight number: "))
+
+for i in range(num6 , num7 , num8):
+    print(i**2)
+
+#metertmete davaleba
+sum = 0
+num = int(input("Input a number: "))
+
+for sum in range(5 , 15):
+    print(sum + num)
+
